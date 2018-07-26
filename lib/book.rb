1bd54f91be81cffc210e82bfb9e1e2ce3239c1ed
soccert <talyagriffin2004@gmail.com>
2018-07-26 20:01:33 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,9 +4,27 @@
 class Book
   
   def initialize()
-    
+  
+  @title = title  
   end
   
   def title
+    
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  
 end
 
