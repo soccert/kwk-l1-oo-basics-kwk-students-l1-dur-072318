8be9e3a4c87259d0b9155128f6cb0db7cf05faee
soccert <talyagriffin2004@gmail.com>
2018-07-26 20:03:50 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -25,6 +25,23 @@ class Book
     @page_count = num
   end
   
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
   
 end
+
+
+
+
+
 
